36a35af0e518c1f402e76a8e9a8f2dd7dbac6d4f
Julie Lang <j_lang1@icloud.com>
2019-04-01 16:04:22 -0700

    new commit for C# step 66

---
+++ ConsoleAppStep66/ConsoleAppStep66/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStep66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input multiplied by 50
+            Console.WriteLine("Enter a Number:");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int product = favNum * 50;
+            Console.WriteLine("Your First number times 50 is: " + product);
+            Console.WriteLine("Press Enter for Next Function!");
+            Console.ReadLine();
+
+            //input plus 25
+            Console.WriteLine("Enter Another Number:");
+            string favoriteNumber1 = Console.ReadLine();
+            int favNum1 = Convert.ToInt32(favoriteNumber1);
+            int total = favNum1 + 25;
+            Console.WriteLine("Your Second Number plus 25 is: " + total);
+            Console.WriteLine("Press Enter for Next Function!");
+            Console.ReadLine();
+
+            //input divided by 12.5
+            Console.WriteLine("Enter a Third Number:");
+            string favoritenumber2 = Console.ReadLine();
+            int favnum2 = Convert.ToInt32(favoritenumber2);
+            double quotient = favnum2 / 12.5;
+            Console.WriteLine("your number divided by 12.5 is: " + quotient);
+            Console.WriteLine("Press Enter for Next Function!");
+            Console.ReadLine();
+
+            //input greater than or less than 50
+            Console.WriteLine("Enter Fourth Number:");
+            string favoritenumber3 = Console.ReadLine();
+            int favnum3 = Convert.ToInt32(favoritenumber3);
+            bool trueOrFalse = favnum3 > 50;
+            Console.WriteLine("if your number is > than 50 it is true, if your number is < 50 it is false.  your number is: " + trueOrFalse.ToString());
+            Console.WriteLine("Press Enter for Next Function!");
+            Console.ReadLine();
+
+            //input divided by 7 leaves reamainer of...
+            Console.WriteLine("Enter a Final Number");
+            string favoriteNumber4 = Console.ReadLine();
+            int favNum4 = Convert.ToInt32(favoriteNumber4);
+            int remainder = favNum4 % 7;
+            Console.WriteLine("Your Number Divided by 7 Leaves a Remainder of: " + remainder);
+            Console.ReadLine();
+
+
+
+
+            
+        }
+    }
+}
---
+++ MathComparisonsOper/MathComparisonsOper/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonsOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // simple addition
+            //int total = 5 + 10;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.ReadLine();
+
+            // adding addtional numbers and lines together
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            // subtraction
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            // multiplication
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            // division
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+
+            // division with decimals
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            // remainder any number divided by two (2) 10 % 2 = 5.0 (even) or 3 / 2 = remainder 1.5 (odd)
+            //modulus is remainder zero (even) or  1 (odd)
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            // boolean true or false
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 72;
+            //bool isWarm = currentTemperature > roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 65;
+            //bool isWarm = currentTemperature >= roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+
+            // does room temp = the current temperature == checks for equality
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+            //bool isWarm = currentTemperature == roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            // does current temperature not equal !=
+            int roomTemperature = 70;
+            int currentTemperature = 70;
+            bool isWarm = currentTemperature != roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
