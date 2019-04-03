87afc209ee21feb77cae87a5db5c2bf78e23967c
Julie Lang <j_lang1@icloud.com>
2019-04-02 17:31:36 -0700

    New Commit

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// && Boolean logical AND operator are both of these true
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+
+            //// || Boolean logical OR operator 
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //// == Boolean logical EQUALS operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //// != Boolean logical DOES NOT EQUAL operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // ^ Boolean logical IF ONE IS TRUE BUT NOT THE OTHER [X OR] operator
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ InsuranceElgibility/InsuranceElgibility/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceElgibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your age:");
+            string yourAge = Console.ReadLine();
+            int Age = Convert.ToInt32(yourAge);
+            Console.WriteLine("You are:" + " " + Age);
+            Console.WriteLine("Please continue to next question.");
+
+            Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\":");
+            string yourDui = Console.ReadLine();
+            bool Dui = bool.Parse(yourDui);
+            Console.WriteLine("Have you ever had a DUI?" + " " + Dui);
+            Console.WriteLine("Please continue to next question.");
+            
+            Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTickets = Console.ReadLine();
+            int Tickets = Convert.ToInt32(yourTickets);
+            
+            Console.ReadLine();
+            Console.WriteLine("Do you qualify for insurance? True or False:");
+            bool trueOrFalse = Age >= 15 && Dui != true && Tickets <=3;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
