f1b7fd88161d659bf9bae0b519c93b51d9ed893f
Julie Lang <j_lang1@icloud.com>
2019-04-03 19:19:47 -0700

    New Commit

---
+++ IfElse/IfElse/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////If Else
+            //int currentTemperature = 70;
+            //int roomTemperature = 72;
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+
+            //Console.ReadLine();
+
+
+            //IF + ElseIf + ElseIf + Else
+            //int currentTemperature = 80;
+            //int roomTemperature = 90;
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than current temperature.");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+
+            //Console.ReadLine();
+
+            //// better version ElseIf turnery operator
+            //int currentTemperature = 70;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "it is not room temp.";
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhhh....something went wrong.");
+            //}
+
+            //Console.ReadLine();
+
+            //turnery operator
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "you have an awesome favorite number!" : "You do not have an awesome favorite number!";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+
+
+
+           
+
+
+
+
+        }
+    }
+}
---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Enter the weight of your package:");
+            decimal packageWeight = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Your package weighs: " + " "  + packageWeight + " " + "lbs");
+
+            //// if else if 
+            string result = packageWeight <= 50 ? "" : "";
+            if (packageWeight >=50)
+            {
+                Console.WriteLine("Your package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine(result);
+           
+            //Box Area: A (area) = W (width) x H (height) x L(length) / 100
+            Console.Write("Please enter the width of your package: ");
+            decimal widthSide = decimal.Parse(Console.ReadLine());
+            Console.Write("Please enter the height of your package: ");
+            decimal heightSide = decimal.Parse(Console.ReadLine());
+            Console.Write("Please enter the length of your package: ");
+            decimal lengthSide = decimal.Parse(Console.ReadLine());
+            Console.Write("Your package is:" + " " + widthSide + "W x " + heightSide + "H x " + lengthSide + "L");
+            //Console.ReadLine();
+            decimal area = (lengthSide * heightSide * lengthSide) / 100;
+            //Console.WriteLine("The sum total of your dimensions is: {0}", area);
+
+            // if else if
+            string result2 = area < 50 ? "" : "";
+            if (area >= 50)
+            {
+                Console.WriteLine(", Your package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine(result2);
+
+            //The sum total of dimensions are then multiplied by the weight and divided by 100
+            decimal value = 0.00M;
+            value = Convert.ToDecimal((area * packageWeight) / 100);
+            Console.WriteLine("The shipping quote for your package is: " + value.ToString("C"));
+            Console.WriteLine("Thank you for your business.");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
